d462d1d66397f80aaa71cd95ebd74424c7d357c4
poojakrp777 <poojakrp777@gmail.com>
2022-08-17 19:54:59 +0530

    Reviewprogram

---
+++ primenum.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -x
+read -p "enter the number:" num
+for (( i=2; i<=$num/2; i++ ))
+do
+x=$(( num%i ))
+if [ $x -eq 0 ]
+then
+echo " $num is not a prime number. "
+fi
+done
+echo " $num is prime number. "
+
